a12af4c9a9607b71ce92ab972de0ccc168408c03
diego.carlos <diego_carlos110@hotmail.com>
2022-01-16 13:19:00 -0300

    Alteraçções

--- main.py
+++ main.py
@@ -9,17 +9,45 @@ class Tela:
         self.nossa_tela = master
         self.nossa_tela.title("Primeira Aula")
 
-        self.lbl = tk.Label(self.nossa_tela, text='Abrir Caixa', font=('Verdana', '16'), relief='raised')
-        self.lbl.pack(pady=20)
-        self.lbl.bind('<Button-1>', self.resposta)
+
+        self.fr = tk.Frame(self.nossa_tela)
+        self.fr2 = tk.Frame(self.nossa_tela)
+
+        self.fr.pack()
+        self.fr2.pack(side=tk.BOTTOM)
+
+        self.lbl = tk.Label(self.fr, text='Estou no frame 1', font=('Arial', '16'))
+        self.lbl.pack()
+
+        self.lbl2 = tk.Label(self.fr2, text='Estou no frame 2', font=('Arial', '16'))
+        self.lbl2.pack(side=tk.LEFT)
+
+        self.lbl3 = tk.Label(self.fr2, text='Estou no frame 2', font=('Arial', '16'))
+        self.lbl3.pack(side=tk.LEFT)
+
+
+        # self.lbl = tk.Label(self.nossa_tela, text='Abrir Caixa', font=('Verdana', '16'), relief='raised')
+        # self.lbl.pack(pady=20)
+        # self.lbl.bind('<Button-1>', self.clique)
+        # self.lbl.bind('<ButtonRelease>', self.resposta)
+
+        # self.btn = tk.Button(self.nossa_tela, text='Clique', font=('Verdana', '16'), command=self.resposta_btn)
+        # self.btn.pack()
 
         # self.lbl = tk.Label(self.nossa_tela, text='Olá Mundo', background='red')
         # self.lbl['font'] = ('verdana', '18')
         # self.lbl.pack(pady = 20, side=tk.LEFT, fill=tk.Y)
-
-    def resposta(self, event):
-        print(event.x, event.y)
-        messagebox.showinfo('Caixa de Mensagem', 'Isso é uma mensagem')
+    # def resposta_btn(self):
+    #     messagebox.showinfo("Caixa de mensagem", "Isso é uma mensage")
+    #
+    # def clique(self, even):
+    #     self.lbl['relief'] = 'sunken'
+    #
+    #
+    # def resposta(self, event):
+    #     print(event.x, event.y)
+    #     messagebox.showinfo('Caixa de Mensagem', 'Isso é uma mensagem')
+    #     self.lbl['relief'] = 'raised'
 
 
 
